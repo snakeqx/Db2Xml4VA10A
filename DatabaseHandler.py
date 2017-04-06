980549489b94cf5cd9a522436236acf60a8a277b
snakeqx <snakeqx@hotmail.com>
2017-04-06 22:38:30 +0800

    Database Handler now can analyze full database. DatabaseHandler.py -->Not complete. EnumFunctionTable.py -->Completed DirectoryHandler.py -->Completed

--- DatabaseHandler.py
+++ DatabaseHandler.py
@@ -4,7 +4,7 @@ import logging
 import sqlite3
 from xml.etree import ElementTree
 from EnumFunctionTable import StringFunctionTables
-import base64
+# import base64
 
 # define the logging config, output in file
 logging.basicConfig(level=logging.DEBUG,
@@ -21,55 +21,73 @@ logging.getLogger('').addHandler(console)
 
 
 class DatabaseHandler:
-    Database_Name = "report.db"
+    Database_Name = 'report.db'
+    System_SerialNo = ""
 
     def __init__(self, file_name):
         if os.path.isfile(file_name):
             self.Database_Name = file_name
         else:
-            logging.error(r"Can not find input file.")
+            logging.error(r'Can not find input file.')
             return
         self.Connection = sqlite3.connect(self.Database_Name)
-        logging.debug(r"Database connected")
+        logging.debug(r'Database connected')
         for str_functions in StringFunctionTables:
             self.read_data(str_functions)
         self.Connection.close()
 
     def read_data(self, function_type):
         if function_type not in StringFunctionTables:
-            logging.error(r"Function Type is not in EnumFunctionTable.py. Please configure it first or check spelling")
+            logging.error(r'Function Type is not in EnumFunctionTable.py. Please configure it first or check spelling')
             return
         logging.debug(function_type + r" is now under query.")
         sql_cursor = self.Connection.cursor()
-        sql_string = r"select data from functionstable where (functionstatus='Success' and NlsID=?)"
+        sql_string = r"select data from functionstable where (functionstatus='Success' and NlsID=?) " \
+                     r"order by starttime desc"
         try:
             sql_cursor.execute(sql_string, [function_type])
             result = sql_cursor.fetchone()
             if result is None:
-                logging.warning(function_type + r" with SUCCESS result has not been found.!!!!!!!!!!")
+                logging.warning(function_type + r' with SUCCESS result has not been found.!!!!!!!!!!')
                 return
             else:
                 while not self.parse_xml(result[0].decode()):
                     result = sql_cursor.fetchone()
                     if result is None:
-                        logging.warning(function_type + r" has no <Content>, please double check!")
+                        logging.debug(function_type + r' has no <Content>, please double check!')
                         break
-                logging.info(r"Read data successfully.")
+                logging.info(r'Read data successfully.')
         except sqlite3.Error as e:
             logging.error(str(e))
         finally:
             sql_cursor.close()
 
-    def parse_xml(self, xml_string):
+    @staticmethod
+    def parse_xml(xml_string):
         xml_tree = ElementTree.fromstring(xml_string)
         for node in xml_tree:
-            if node.tag == "Chapter":
-                content_tag = node.find("Downloadable/Content")
+            if node.tag == 'Chapter':
+                content_tag = node.find(r'Downloadable/Content')
                 if content_tag is not None:
-                    logging.info(r"<Content> has been found.")
+                    logging.info(r'<Content> has been found.')
                     return True
-        logging.debug(r"It seems this data has no <Content> node. Skip")
+        logging.debug(r'It seems this data has no <Content> node. Skip')
         return False
+
+    def get_serial_number(self):
+        logging.debug(r"It is now query for system serial number.")
+        sql_cursor = self.Connection.cursor()
+        sql_string = r"select data from servicereportstable order by starttime desc limit 1"
+        try:
+            sql_cursor.execute(sql_string)
+            result = sql_cursor.fetchone()
+            if result is None:
+                logging.warning(r"Can not find serial number!")
+                return
+        except sqlite3.Error as e:
+            logging.error(str(e))
+        finally:
+            sql_cursor.close()
 
 
 if __name__ == '__main__':
